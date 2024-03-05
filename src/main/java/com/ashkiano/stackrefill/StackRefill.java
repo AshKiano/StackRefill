90c16186d45696a162ef34a7a34522343f142169
Josef Vyskoèil <josef.vyskocil@cleverlance.com>
2024-03-05 17:12:54 +0100

    version 1.3

--- src/main/java/com/ashkiano/stackrefill/StackRefill.java
+++ src/main/java/com/ashkiano/stackrefill/StackRefill.java
@@ -14,7 +14,12 @@ import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 import java.util.UUID;
 
@@ -29,6 +34,9 @@ public class StackRefill extends JavaPlugin implements Listener, CommandExecutor
         getServer().getPluginManager().registerEvents(this, this);
         this.getCommand("stackrefill").setExecutor(this);
 
+        Metrics metrics = new Metrics(this, 20835);
+
+        checkForUpdates();
     }
 
     @EventHandler
@@ -64,6 +72,12 @@ public class StackRefill extends JavaPlugin implements Listener, CommandExecutor
     }
 
     private void refillStack(PlayerInventory inventory, Material itemType) {
+
+        //vyjimka kvuli infinitestew pluginu
+        if (itemType == Material.RABBIT_STEW) {
+            return;
+        }
+
         for (int i = 0; i < 9; i++) {
             ItemStack item = inventory.getItem(i);
             if (item == null || item.getType() != itemType) {
@@ -115,14 +129,54 @@ public class StackRefill extends JavaPlugin implements Listener, CommandExecutor
 
         if (enabledPlayers.contains(playerUUID.toString())) {
             enabledPlayers.remove(playerUUID.toString());
-            player.sendMessage("Stack refill feature is now disabled.");
+            player.sendMessage("Stack refilling is turned off.");
         } else {
             enabledPlayers.add(playerUUID.toString());
-            player.sendMessage("Stack refill feature is now enabled.");
+            player.sendMessage("Stack refilling is turned on.");
         }
 
         config.set("enabledForPlayers", enabledPlayers);
         saveConfig();
         return true;
+    }
+
+    private void checkForUpdates() {
+        try {
+            String pluginName = this.getDescription().getName();
+            URL url = new URL("https://www.ashkiano.com/version_check.php?plugin=" + pluginName);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                String jsonResponse = response.toString();
+                JSONObject jsonObject = new JSONObject(jsonResponse);
+                if (jsonObject.has("error")) {
+                    this.getLogger().warning("Error when checking for updates: " + jsonObject.getString("error"));
+                } else {
+                    String latestVersion = jsonObject.getString("latest_version");
+
+                    String currentVersion = this.getDescription().getVersion();
+                    if (currentVersion.equals(latestVersion)) {
+                        this.getLogger().info("This plugin is up to date!");
+                    } else {
+                        this.getLogger().warning("There is a newer version (" + latestVersion + ") available! Please update!");
+                    }
+                }
+            } else {
+                this.getLogger().warning("Failed to check for updates. Response code: " + responseCode);
+            }
+        } catch (Exception e) {
+            this.getLogger().warning("Failed to check for updates. Error: " + e.getMessage());
+        }
     }
 }
